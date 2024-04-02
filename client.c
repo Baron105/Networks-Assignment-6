dc80a07f613dfc89b58bd776988640b856b1a982
TheMockingJay1013 <navaneeth1013@gmail.com>
2024-04-02 16:37:44 +0530

    trial

--- client.c
+++ client.c
@@ -4,7 +4,8 @@
 #include <sys/socket.h>
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
-#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
@@ -39,12 +40,43 @@ int main() {
     memcpy(eth->h_source, (unsigned char[]){0x00, 0x08, 0xA1, 0x8E, 0xE4, 0x52}, ETH_ALEN); // Source MAC address
     eth->h_proto = htons(ETH_P_IP); // EtherType for IP
 
+    // Construct the IP header
+    struct iphdr *ip = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+    ip->ihl = 5;
+    ip->version = 4;
+    ip->tos = 0;
+    ip->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr) + strlen("Hello"));
+    ip->id = htons(54321);
+    ip->frag_off = 0;
+    ip->ttl = 64;
+    ip->protocol = IPPROTO_TCP;
+    ip->check = 0; // Will be filled in by the kernel
+    ip->saddr = inet_addr("192.168.1.1"); // Source IP address
+    ip->daddr = inet_addr("192.168.1.2"); // Destination IP address
+
+    // Construct the TCP header
+    struct tcphdr *tcp = (struct tcphdr *)(buffer + sizeof(struct ethhdr) + sizeof(struct iphdr));
+    tcp->source = htons(12345); // Source port
+    tcp->dest = htons(12345); // Destination port
+    tcp->seq = htonl(1); // Sequence number
+    tcp->ack_seq = 0; // Acknowledgment number
+    tcp->doff = 5; // Data offset
+    tcp->fin = 0;
+    tcp->syn = 0;
+    tcp->rst = 0;
+    tcp->psh = 0;
+    tcp->ack = 0;
+    tcp->urg = 0;
+    tcp->window = htons(5840); // Window size
+    tcp->check = 0; // Will be filled in by the kernel
+    tcp->urg_ptr = 0;
+
     // Add the "Hello" message as payload
-    char *data = (char *)(buffer + sizeof(struct ethhdr));
+    char *data = (char *)(buffer + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr));
     strcpy(data, "Hello");
 
     // Send the packet
-    packet_len = sendto(sockfd, buffer, sizeof(struct ethhdr) + strlen("Hello"), 0, (struct sockaddr *)&sa, sizeof(struct sockaddr_ll));
+    packet_len = sendto(sockfd, buffer, sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr) + strlen("Hello"), 0, (struct sockaddr *)&sa, sizeof(struct sockaddr_ll));
     if (packet_len == -1) {
         perror("sendto");
         exit(EXIT_FAILURE);
@@ -55,4 +87,3 @@ int main() {
     close(sockfd);
     return 0;
 }
-
